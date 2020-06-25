8cec361366ab13dadc1799142a2b8a545d4d008c
Hussein El Feky <hussein.feky@gmail.com>
2020-06-25 14:00:02 +0200

    Add end-to-end test

--- starter/app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
+++ starter/app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
@@ -1,16 +1,31 @@
 package com.udacity.project4
 
 import android.app.Application
+import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.espresso.Espresso.closeSoftKeyboard
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import com.udacity.project4.locationreminders.RemindersActivity
 import com.udacity.project4.locationreminders.data.ReminderDataSource
 import com.udacity.project4.locationreminders.data.local.LocalDB
 import com.udacity.project4.locationreminders.data.local.RemindersLocalRepository
 import com.udacity.project4.locationreminders.reminderslist.RemindersListViewModel
 import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
+import com.udacity.project4.util.DataBindingIdlingResource
+import com.udacity.project4.util.DataGenerator
+import com.udacity.project4.util.EspressoIdlingResource
+import com.udacity.project4.util.monitorActivity
 import kotlinx.coroutines.runBlocking
+import org.junit.After
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
@@ -27,6 +42,8 @@ class RemindersActivityTest : AutoCloseKoinTest() {
 
     private lateinit var repository: ReminderDataSource
     private lateinit var appContext: Application
+
+    private val dataBindingIdlingResource = DataBindingIdlingResource()
 
     /**
      * As we use Koin as a Service Locator Library to develop our code, we'll also use Koin to test our code.
@@ -69,7 +86,62 @@ class RemindersActivityTest : AutoCloseKoinTest() {
         }
     }
 
+    @Before
+    fun registerIdlingResources() {
+        IdlingRegistry.getInstance().apply {
+            register(EspressoIdlingResource.countingIdlingResource)
+            register(dataBindingIdlingResource)
+        }
+    }
 
-//    TODO: add End to End testing to the app
+    @After
+    fun unregisterIdlingResources() {
+        IdlingRegistry.getInstance().apply {
+            unregister(EspressoIdlingResource.countingIdlingResource)
+            unregister(dataBindingIdlingResource)
+        }
+    }
 
+    @Test
+    fun launchRemindersActivityWithOneReminder() {
+        val reminder = DataGenerator.getReminderDto1()
+        runBlocking {
+            repository.saveReminder(reminder)
+        }
+
+        val scenario = ActivityScenario.launch(RemindersActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(scenario)
+
+        onView(withText(reminder.title)).check(matches(isDisplayed()))
+        onView(withText(reminder.description)).check(matches(isDisplayed()))
+        onView(withText(reminder.location)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun addReminderAndNavigateBack() {
+        val scenario = ActivityScenario.launch(RemindersActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(scenario)
+
+        // ReminderListFragment: Add new reminder.
+        onView(withId(R.id.noDataTextView)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+        onView(withId(R.id.addReminderFAB)).perform(click())
+
+        // SaveReminderFragment: Select location.
+        onView(withId(R.id.selectedLocation)).perform(click())
+
+        // SelectLocationFragment: Set location.
+        onView(withId(R.id.map)).perform(click())
+        onView(withId(R.id.btn_save)).perform(click())
+
+        // SaveReminderFragment: Enter reminder title and description, then save.
+        onView(withId(R.id.reminderTitle)).perform(typeText("Title"))
+        onView(withId(R.id.reminderDescription)).perform(typeText("Description"))
+        closeSoftKeyboard()
+        onView(withId(R.id.saveReminder)).perform(click())
+
+        // ReminderListFragment: Verify that the reminder is displayed, and "No Data" text view is hidden.
+        onView(withId(R.id.noDataTextView)).check(matches(withEffectiveVisibility(Visibility.GONE)))
+        onView(withText("Title")).check(matches(isDisplayed()))
+        onView(withText("Description")).check(matches(isDisplayed()))
+    }
 }
